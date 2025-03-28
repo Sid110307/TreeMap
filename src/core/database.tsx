fd52d56a75c6cd7154c839b3709bc488b452676d
Siddharth Praveen Bharadwaj <srsfool@gmail.com>
2025-03-28 20:53:21 +0530

    Removed redundant auth stuff

--- src/core/database.tsx
+++ src/core/database.tsx
@@ -19,14 +19,7 @@ class DatabaseManager {
 		this.supabaseDB = createClient(
 			process.env.EXPO_PUBLIC_SUPABASE_URL || "",
 			process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
-			{
-				auth: {
-					storage: AsyncStorage,
-					autoRefreshToken: true,
-					persistSession: true,
-					detectSessionInUrl: false,
-				},
-			},
+			{ auth: { storage: AsyncStorage } },
 		);
 
 		const sql = `
