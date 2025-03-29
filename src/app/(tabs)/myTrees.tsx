39bfbf10bf0938660e54347b3a5697ca621d8aa3
Siddharth Praveen Bharadwaj <srsfool@gmail.com>
2025-03-29 22:52:01 +0530

    UI updates

--- src/app/(tabs)/myTrees.tsx
+++ src/app/(tabs)/myTrees.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { ActivityIndicator, View } from "react-native";
+import Toast from "react-native-toast-message";
 
 import { FlashList } from "@shopify/flash-list";
-import { useFocusEffect, useRouter } from "expo-router";
+import { useFocusEffect } from "expo-router";
 
-import Card from "../../components/card";
 import DataEntryItem from "../../components/dataEntryItem";
-import Text from "../../components/text";
+import Text, { HeadText } from "../../components/text";
 
 import colors from "../../core/colors";
 import { databaseManager } from "../../core/database";
+import { useUserState } from "../../core/state";
 import { DataEntry } from "../../core/types";
 
 export default () => {
-	const router = useRouter();
+	const { user } = useUserState();
 
 	const [loading, setLoading] = React.useState(true);
 	const [data, setData] = React.useState<DataEntry[]>([]);
@@ -21,14 +22,29 @@ export default () => {
 	useFocusEffect(
 		React.useCallback(() => {
 			setLoading(true);
+			databaseManager
+				.query("SELECT * FROM TreeMap ORDER BY created_at DESC")
+				.then(setData)
+				.catch(error => {
+					console.error(error);
+					Toast.show({
+						type: "error",
+						text1: "Error Fetching Data",
+						text2: "An error occurred while fetching your data.",
+					});
+				})
+				.finally(() => setLoading(false));
+
 			databaseManager.supabaseDB
 				?.from("TreeMap")
 				.select("*")
+				.eq("user_id", user.id)
 				.order("created_at", { ascending: false })
 				.then(({ data, error }) => {
 					if (error) {
 						console.error(error);
 						setLoading(false);
+
 						return;
 					}
 
@@ -39,36 +55,35 @@ export default () => {
 	);
 
 	return (
-		<View style={{ flex: 1, padding: 16 }}>
-			<Card title="My Trees" style={{ flex: 1 }}>
-				<FlashList
-					data={data}
-					refreshing={loading}
-					estimatedItemSize={120}
-					ListEmptyComponent={() => (
-						<View
-							style={{
-								height: "100%",
-								justifyContent: "center",
-								alignItems: "center",
-								marginVertical: 16,
-							}}
-						>
-							{loading ? (
-								<ActivityIndicator size="large" color={colors.primary} />
-							) : (
-								<Text style={{ textAlign: "center" }}>
-									No trees found. Add a tree to get started!
-								</Text>
-							)}
-						</View>
-					)}
-					renderItem={({ item }: { item: DataEntry }) => (
-						<DataEntryItem item={item} hasMore={data!.length > 1} hideAddedBy />
-					)}
-					keyExtractor={item => item.id}
-				/>
-			</Card>
+		<View style={{ flex: 1, padding: 16, gap: 8 }}>
+			<HeadText>My Trees ({user.totalIdentified})</HeadText>
+			<FlashList
+				data={data}
+				refreshing={loading}
+				estimatedItemSize={120}
+				ListEmptyComponent={() => (
+					<View
+						style={{
+							height: "100%",
+							justifyContent: "center",
+							alignItems: "center",
+							marginVertical: 16,
+						}}
+					>
+						{loading ? (
+							<ActivityIndicator size="large" color={colors.primary} />
+						) : (
+							<Text style={{ textAlign: "center" }}>
+								No trees found. Add a tree to get started!
+							</Text>
+						)}
+					</View>
+				)}
+				renderItem={({ item }: { item: DataEntry }) => (
+					<DataEntryItem item={item} hasMore={data!.length > 1} hideAddedBy />
+				)}
+				keyExtractor={item => item.id}
+			/>
 		</View>
 	);
 };
